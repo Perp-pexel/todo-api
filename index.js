ef33c1f9e896cc907e53c18c86a17ab4ae01b8ba
Perp-pexel <perpaman44@gmail.com>
2024-09-30 15:58:28 +0000

    todo

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from 'express'
+
+// Create an express app
+const app = express();
+
+// Define  routes
+app.get('/hello', function(req, res, next) {
+    console.log(req.headers);
+    res.json('You visited the hello endpoint!');
+});
+
+app.get('/goodbye', function (req, res, next) {
+    console.log(req.query);
+    res.json('Same to you');
+});
+
+
+// Listen for incoming reqeusts
+    app.listen(3000, function() {
+        console. log('App is listening on port 3000');
+
+    });
